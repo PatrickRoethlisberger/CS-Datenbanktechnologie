25446731bdd76b6cb50d2053837eac57aa27469b
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-02-06 16:29:56 +0100

    Add propertiers to the orders table

--- database/migrations/2021_02_01_200028_create_orders_table.php
+++ database/migrations/2021_02_01_200028_create_orders_table.php
@@ -16,7 +16,10 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('subscription_id')->constrained()->onDelete('cascade');
+            $table->foreignId('plan_id')->constrained();
             $table->string('billing_number', 32);
+            $table->date('billing_from');
+            $table->date('billing_until');
             $table->timestamp('paid_at')->nullable();
             $table->timestamps();
         });
