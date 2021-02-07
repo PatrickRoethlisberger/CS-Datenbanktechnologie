70a2ff041994406472c71d377e9fed0550b58914
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-02-07 21:26:19 +0100

    Rename guest layout to modal layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-modal-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -83,4 +83,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-modal-layout>
