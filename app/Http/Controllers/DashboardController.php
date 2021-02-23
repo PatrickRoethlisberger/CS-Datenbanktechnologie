f9b664f5a52ca01018c4d89a2f8ca0e76c2e2fcc
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-02-23 16:06:19 +0100

    Update DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
 class DashboardController extends Controller
 {
     public function index()
     {
-        dd(Auth::User()->subscription->plan->duration);
         return view('dashboard');
     }
 }
