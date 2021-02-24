0f751a2538c0852f7aeb44ffb0aaf150867ccad9
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-02-24 10:09:18 +0100

    Add admin middleware

---
+++ app/Http/Middleware/Administrator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Administrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ( ! $request->user() || ! empty($request->user()->roles) || ! $request->user()->roles->contains("validated")) {
+            return abort(403, 'FORBIDDEN - keine Admin Rechte');
+        }
+
+        return $next($request);
+    }
+}
