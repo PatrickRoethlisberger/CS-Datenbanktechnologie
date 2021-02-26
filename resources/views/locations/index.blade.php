f5d2557e3628cdad962a37d6affe11fb06a66139
Patrick Röthlisberger <9606082+PatrickRoethlisberger@users.noreply.github.com>
2021-02-26 09:31:50 +0100

    Fix find and replace mistake
    lol

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white blocation-b blocation-gray-200">
+                <div class="p-6 bg-white border-b border-gray-200">
                     @if ($locations->count())
                         <div class="flex items-center justify-end mt-4">
                             <x-button-link class="mt-4" :href="route('locations.create')">
@@ -17,7 +17,7 @@
                         </div>
                         <div class="overflow-x-auto mt-6">
 
-                            <table class="table-auto blocation-collapse w-full">
+                            <table class="table-auto border-collapse w-full">
                                 <thead>
                                     <tr class="rounded-lg text-sm font-medium text-gray-700 text-left">
                                         <th class="px-4 py-2 bg-gray-50 "">Standortnummer</th>
@@ -31,7 +31,7 @@
                                 </thead>
                                 <tbody class="text-sm font-normal text-gray-700">
                                     @foreach ($locations as $location)
-                                        <tr class="hover:bg-gray-100 blocation-b blocation-gray-200 py-10">
+                                        <tr class="hover:bg-gray-100 border-b border-gray-200 py-10">
                                             <td class="px-4 py-4">{{ $location->id }}</td>
                                             <td class="px-4 py-4">{{ $location->description }}</td>
                                             <td class="px-4 py-4">{{ $location->streetname }}</td>
